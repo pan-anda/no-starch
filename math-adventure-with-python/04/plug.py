47e39a9244762239a55b2bb7c08e62a3a7a40b1a
panda <panda@gmail.com>
2022-04-12 14:24:17 +0800

    0412

--- math-adventure-with-python/04/plug.py
+++ math-adventure-with-python/04/plug.py
@@ -14,14 +14,28 @@ def equation(a,b,c,d):
 print(equation(2,5,0,13))
 
 
+# polynomials.py
+from math import sqrt
+def quad(a,b,c):
+    x1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
+    x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
+    return x1, x2
+
+print(quad(2,7,-15))
 
 
+# solve cubic equation
+def g(x):
+    return 6*x**3 + 31*x**2 +3*x - 10
 
-
-
-
-
-
+def plug():
+    x = -100
+    while x < 100:
+        if g(x) == 0:
+            print("x=",x)
+        x += 1
+    print("done.")
+plug()
 
 
 
